1ebc3f5694ab346f4c9e011092fdce0cb2ce59c5
Darya Pryadchenko <d.pryadchenko@intabia.ru>
2019-06-10 08:50:24 +0700

    inrefs #11174: Первая версия страницы с редактором кода

--- webapp/src/DOMJudgeBundle/Controller/Team/SubmissionController.php
+++ webapp/src/DOMJudgeBundle/Controller/Team/SubmissionController.php
@@ -3,20 +3,31 @@
 namespace DOMJudgeBundle\Controller\Team;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query\Expr\Join;
 use DOMJudgeBundle\Controller\BaseController;
 use DOMJudgeBundle\Entity\Judging;
 use DOMJudgeBundle\Entity\Problem;
+use DOMJudgeBundle\Entity\Language;
+use DOMJudgeBundle\Entity\Submission;
 use DOMJudgeBundle\Entity\Testcase;
 use DOMJudgeBundle\Form\Type\SubmitProblemType;
 use DOMJudgeBundle\Service\DOMJudgeService;
 use DOMJudgeBundle\Service\SubmissionService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\Language;
+
+//use Dompdf\Options;
+use PdfParser;
+
 
 /**
  * Class SubmissionController
@@ -187,5 +198,158 @@ class SubmissionController extends BaseController
         } else {
             return $this->render('@DOMJudge/team/submission.html.twig', $data);
         }
+    }
+
+    /**
+     * @Route("/{submission}/edit-source", name="jury_submission_edit_source")
+     * @param Request $request
+     * @param Submission $submission
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editorAction(Request $request, Submission $submission)
+//        int $probId, string $langId)
+//    @Route("/submission/source/{probId}/{langId}", name="code_editor", requirements={"probId": "\d+", "langId": "\S+"})
+    {
+//        /** @var Language $languages */
+        $languages = $this->em->getRepository(Language::class)->findBy(['allowSubmit' => true]);
+//
+//        /** @var Problem $problem */
+        $problem = $this->em->getRepository(Problem::class)->find(1);
+//
+//        return $this->render('@DOMJudge/team/submission_source.html.twig', [
+//            'problem' => $problem,
+//            'languages' => $languages,
+//            'curLang' => $langId,
+//        ]);
+
+
+//        if (!$this->dj->getUser()->getTeam() || !$this->dj->checkrole('team')) {
+//            $this->addFlash('danger', 'You cannot re-submit code without being a team.');
+//            return $this->redirectToLocalReferrer($this->router, $request, $this->generateUrl('jury_submission',
+//                ['submitId' => $submission->getSubmitid()]));
+//        }
+
+//        /** @var SubmissionFileWithSourceCode[] $files */
+//        $files = $this->em->createQueryBuilder()
+//            ->from('DOMJudgeBundle:SubmissionFileWithSourceCode', 'file')
+//            ->select('file')
+//            ->andWhere('file.submission = :submission')
+////            ->setParameter(':submission', $submission)
+//            ->orderBy('file.rank')
+//            ->getQuery()
+//            ->getResult();
+
+//        $data = [
+//            'problem' => $submission->getProblem(),
+//            'language' => $submission->getLanguage(),
+//            'entry_point' => $submission->getEntryPoint(),
+//        ];
+//
+//        $data = [
+//            'problem' => $problem,
+//            'language' => $languages,
+////            'entry_point' => $submission->getEntryPoint(),
+//        ];
+
+//        foreach ($files as $file) {
+//            $data['source' . $file->getRank()] = $file->getSourcecode();
+//        }
+
+//        $formBuilder = $this->createFormBuilder($data)
+        $formBuilder = $this->createFormBuilder()
+            ->add('problem', EntityType::class, [
+                'class' => 'DOMJudgeBundle\Entity\Problem',
+                'choice_label' => 'name',
+                'query_builder' => function (EntityRepository $er)  {
+                    return $er->createQueryBuilder('p')
+                        ->join('p.contest_problems', 'cp')
+                        ->andWhere('cp.allowSubmit = 1')
+                        ->andWhere('cp.contest = :contest')
+//                        ->setParameter(':contest', $submission->getContest())
+                        ->orderBy('p.name');
+                },
+            ])
+            ->add('language', EntityType::class, [
+                'class' => 'DOMJudgeBundle\Entity\Language',
+                'choice_label' => 'name',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('lang')
+                        ->andWhere('lang.allowSubmit = 1')
+                        ->orderBy('lang.name');
+                }
+            ])
+            ->add('entry_point', TextType::class, [
+                'label' => 'Optional entry point',
+                'required' => false,
+            ])
+            ->add('submit', SubmitType::class);
+
+//        foreach ($files as $file) {
+//            $formBuilder->add('source' . $file->getRank(), TextareaType::class);
+//        }
+
+        $form = $formBuilder->getForm();
+//
+//        // Handle the form if it is submitted
+        $form->handleRequest($request);
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $submittedData = $form->getData();
+//
+//            /** @var UploadedFile[] $filesToSubmit */
+//            $filesToSubmit = [];
+//            $tmpdir        = $this->dj->getDomjudgeTmpDir();
+//            foreach ($files as $file) {
+//                if (!($tmpfname = tempnam($tmpdir, "edit_source-"))) {
+//                    throw new ServiceUnavailableHttpException(null, "Could not create temporary file.");
+//                }
+//                file_put_contents($tmpfname, $submittedData['source' . $file->getRank()]);
+//                $filesToSubmit[] = new UploadedFile($tmpfname, $file->getFilename(), null, null, null, true);
+//            }
+//
+//            $team = $this->dj->getUser()->getTeam();
+//            /** @var Language $language */
+//            $language   = $submittedData['language'];
+//            $entryPoint = $submittedData['entry_point'];
+//            if ($language->getRequireEntryPoint() && $entryPoint === null) {
+//                $entryPoint = '__auto__';
+//            }
+//            $submittedSubmission = $this->submissionService->submitSolution(
+//                $team,
+//                $submittedData['problem'],
+////                $submission->getContest(),
+//                $language,
+//                $filesToSubmit,
+////                $submission->getOriginalSubmission() ?? $submission,
+//                $entryPoint,
+//                null,
+//                null
+////                $message
+//            );
+//
+//            foreach ($filesToSubmit as $file) {
+//                unlink($file->getRealPath());
+//            }
+
+//            if (!$submission) {
+//                $this->addFlash('danger', $message);
+//                return $this->redirectToRoute('jury_submission', ['submitId' => $submission->getSubmitid()]);
+//            }
+
+//            return $this->redirectToRoute('jury_submission', ['submitId' => $submittedSubmission->getSubmitid()]);
+//        }
+
+        $submission = new Submission();
+//        $submission->submitid = 1;
+        return $this->render('@DOMJudge/team/submission_edit_source.html.twig', [
+            'submission' => $submission,
+            'submission' => null,
+//            'files' => $files,
+            'form' => $form->createView(),
+//            'selected' => $request->query->get('rank'),
+            'selected' => null,
+        ]);
+
+
+
     }
 }
